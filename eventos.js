7fbba611bae3aad1609eb4f5770651017024dc41
FColla1891 <141262470+FColla1891@users.noreply.github.com>
2023-08-07 22:13:41 -0300

    5px fondo verde

--- eventos.js
+++ eventos.js
@@ -1,3 +1,11 @@
-function mostrarAlerta() {
-    alert("Hola!");
+function saludar() {
+    alert("Hola desde eventos.js!");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var miDiv = document.querySelector("div");
+
+    miDiv.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
